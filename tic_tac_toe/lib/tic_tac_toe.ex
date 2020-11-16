ba02c0fd864ef16887dfc3af7ac196574ba1628c
Tsutomu Kuroda <t-kuroda@oiax.jp>
2020-11-16 23:55:44 +0900

    [ch26] Reset files

--- tic_tac_toe/lib/tic_tac_toe.ex
+++ tic_tac_toe/lib/tic_tac_toe.ex
@@ -1,18 +1,2 @@
 defmodule TicTacToe do
-  @moduledoc """
-  Documentation for `TicTacToe`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TicTacToe.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
