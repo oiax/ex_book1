cf6e91d2d92e4acbc59e6f84f8299c06423e1025
Tsutomu Kuroda <t-kuroda@oiax.jp>
2020-11-17 12:58:21 +0900

    [ch26] mix format

--- tic_tac_toe/config/config.exs
+++ tic_tac_toe/config/config.exs
@@ -3,5 +3,4 @@ import Config
 config :logger,
   backends: [{LoggerFileBackend, :error_log}]
 
-config :logger, :error_log,
-  path: "logs/error.log"
+config :logger, :error_log, path: "logs/error.log"
